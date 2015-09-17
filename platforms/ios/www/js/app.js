66d32aaa62df0910517a6856bedd4681fcce605a
Bao <bb@stilbezirk.de>
2015-09-17 16:43:08 +0200

    user profile instagram

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -6,8 +6,20 @@ define([
     'backbone',
     'router', // Request router.js
 ], function($,jm, _, Backbone, Router){
+    var app = {
+        API : "/api"
+    }
     var initialize = function(){
         // Pass in our Router module and call it's initialize function
+        $.mobile.linkBindingEnabled = false;
+        $.mobile.hashListeningEnabled = true;
+
+        $(document).on("pagebeforecreate",function(){
+            $.mobile.loading('show');
+        });
+        $(document).on("pagecreate",function(){
+            $.mobile.loading('hide');
+        });
 
         Router.initialize();
     };
